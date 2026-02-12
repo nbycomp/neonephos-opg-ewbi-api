79c3866bd0e09b3323a7fd5f3a1e14df837fba24
Patricia Naranjo Gallardo <patricia.naranjo@nearbycomputing.com>
2026-02-12 12:42:33 +0100

    Revert "fix error for missing zone info"
    This reverts commit b1d5afc62f94d19fdd9c4d7e415f3ad6bc6a05d6.

--- pkg/metastore/federation.go
+++ pkg/metastore/federation.go
@@ -44,13 +44,14 @@ func (f *Federation) updatek8sCustomResource(fed *opgv1beta1.Federation) *opgv1b
 }
 
 func federationFromK8sCustomResource(fed *opgv1beta1.Federation) (*Federation, error) {
-	offeredZones := []models.ZoneDetails{}
-	for _, z := range fed.Spec.OfferedAvailabilityZones {
-		offeredZones = append(offeredZones, models.ZoneDetails{
-			ZoneId:           z.ZoneId,
-			Geolocation:      z.Geolocation,
+	offeredZones := make([]models.ZoneDetails, len(fed.Spec.OfferedAvailabilityZones))
+	for i, z := range fed.Spec.OfferedAvailabilityZones {
+		offeredZones[i] = models.ZoneDetails{
+			ZoneId: z.ZoneId,
+			Geolocation: z.Geolocation,
 			GeographyDetails: z.GeographyDetails,
-		})
+		}
+		
 	}
 
 	return &Federation{
